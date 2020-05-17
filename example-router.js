a30510fac37f4c4d33aa2ab60613b7032517dbc3
Fernando O <fer@f-ochoa.com>
2020-05-16 21:22:36 -0500

    Updated With My Own Code - Fork MIT.
    Hope You Enjoy This, It Is An M.I.T. Exercise For Assignment!

--- example-router.js
+++ example-router.js
@@ -1,4 +1,5 @@
 function exampleRouteHandler(req, res) {
-  res.send("Goodbye Earthling!");
+  res.send("<p>Hello Earthling! Hello World! Hello M.I.T.! GoodBye Earthling!</p>");
+  res.send("<p>P.S. Major Tom!</p>");
 }
 module.exports = exampleRouteHandler;
