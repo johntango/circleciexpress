edd3f5a6ebadefcf497c363d81f3c2a98c50b297
unknown <micposso@gmail.com>
2020-05-17 21:26:00 -0400

    change string in a branch

--- example-router.js
+++ example-router.js
@@ -1,4 +1,4 @@
 function exampleRouteHandler(req, res) {
-  res.send("Hello, this is my first circle CI push");
+  res.send("Hello, this is my third circle CI push testing a branch");
 }
 module.exports = exampleRouteHandler;
